643af252a79e05c3ac8678a092848b24040d68d9
Mo7ammad Jamal <mj266501@gmail.com>
2025-04-03 16:05:27 +0300

    Fixing the initial route to be the login page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\TestingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
